a9ee71cb9ad1d89f18614a09e5751ef4e8cad50f
maksympetyak <petyak.mi@gmail.com>
2025-02-16 12:44:48 +0100

    reverts the changes around configs to the latest working one

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb'
+    }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Prevent PSPDFKit from being bundled on the server
+      config.externals = [...config.externals, 'pspdfkit'];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
